878adfd0ae09853e5e42a043fdbe846a7fe6fbd0
Jason Liu <hahaliu2001@gmail.com>
2023-03-10 15:22:55 -0500

    first

---
+++ run_flask_without_docker.py
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# how to run i:
+# source run_flask_without_docker.py
+
+# in my machine, python3 alias to python3.7
+# virtual environment
+python3 -m venv .venv
+source .venv/bin/activate
+
+python -m pip  install flask
+python -m pip  install Flask
+
+python -m pip freeze > requirements.txt
+
+export FLASK_APP=src/main.py
+python -m flask run --host=0.0.0.0 --port=5000
